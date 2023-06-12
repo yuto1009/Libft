55922e38a60c6132357bf03996b25ce5f943ccf7
yuto1009 <bmwb71009@gmail.com>
2023-06-12 12:00:07 +0900

    uploading jun12thhome

--- may21th42/ft_strnstr.c
+++ may21th42/ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yutoendo <yutoendo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 19:11:15 by yuendo            #+#    #+#             */
-/*   Updated: 2023/05/21 13:25:11 by yuendo           ###   ########.fr       */
+/*   Updated: 2023/06/12 11:59:33 by yutoendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	needle_len;
 
-	if (*needle == '\0')
+	if (!haystack)
+		return (NULL);
+	if (!needle || *needle == '\0')
 		return ((char *)haystack);
 	needle_len = ft_strlen(needle);
 	while (*haystack && len >= needle_len)
@@ -28,26 +30,3 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
-
-// char *ft_strnstr(const char *haystack, const char *needle, size_t len){
-//     const char *n;
-//     char *first_occur;
-//     size_t i;
-//     if (*needle == '\0')
-//         return ((char *)haystack);
-// 	while(*haystack && len){
-//         n = needle;
-//         first_occur = (char *)haystack;
-//         i = 0;
-//         while(*haystack && *n && *haystack == *n && i < len){
-//             haystack++;
-//             n++;
-//             i++;
-//         }
-//         if (*n == '\0')
-//             return (first_occur);
-//         haystack++;
-//         len--;
-//     }
-//     return (NULL);
-// }
